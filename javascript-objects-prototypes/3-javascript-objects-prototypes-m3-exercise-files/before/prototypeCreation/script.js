f6e6e9a90f107551b1639413733476dbee2d67be
Anton Bredl <antonbr13@gmail.com>
2017-03-03 17:07:36 -0800

    finished with this course

--- javascript-objects-prototypes/3-javascript-objects-prototypes-m3-exercise-files/before/prototypeCreation/script.js
+++ javascript-objects-prototypes/3-javascript-objects-prototypes-m3-exercise-files/before/prototypeCreation/script.js
@@ -3,3 +3,28 @@
 'use strict';
 
 //Start coding here
+
+function Animal(voice) {
+   this.voice = voice || 'grunt';
+}
+
+Animal.prototype.speak = function() {
+   display(this.voice);
+};
+
+
+function Cat(name, color) {
+   Animal.call(this);
+   this.name = name;
+   this.color = color;
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+var fluffy = new Cat('fluffy', 'white');
+
+fluffy.speak();
+
+display(fluffy.__proto__.__proto__);
+display(Object.keys(fluffy));
